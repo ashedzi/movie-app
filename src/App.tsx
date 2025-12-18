088fc93e136c6d7f27a2b5172030aedb3efffb7c
Ashedzi Solomon <solomonashe84@gmail.com>
2025-12-18 16:10:38 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen w-screen bg-blue-500 text-white flex items-center justify-center">
+      Tailwind is working! 🎉
+    </div>
+    </>
+  )
+}
+
+export default App

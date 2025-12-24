fddf60ff58aa7bdc03a04033bfdf623378a0d8b3
Ashedzi Solomon <solomonashe84@gmail.com>
2025-12-24 11:12:43 -0600

    Add some additional functions to the searchbar component

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -2,13 +2,24 @@ import {useState} from 'react';
 import Button from './button';
 
 interface Props {
-    children: React.ReactNode;
+    OnSearch: (searchTerm: string) => void;
 }
 
-function SearchBar({children}: Props) {
+function SearchBar({OnSearch}: Props) {
+    const[searchTerm, setSearchTerm] = useState('null');
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        OnSearch(searchTerm);
+    }
+
+    const setNewValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    };
+
     return (
-       <form className='flex gap-2'>
-        <input type="text" placeholder="Search here..." className="border border-white rounded-md px-2 py-1 w-full text-white text-xs"></input>
+       <form onSubmit={handleSubmit} className='flex gap-2'>
+        <input type="text" value={searchTerm} onChange={setNewValue} placeholder="Search here..." className="border border-white rounded-md px-2 py-1 w-full text-white text-xs" />
         <Button type="submit">Search</Button>
        </form>
     )
